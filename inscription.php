316a76ccb0c6dbc66d7691de240a0e7172871aeb
Jonathan <jonathan.schmidt.13@gmail.com>
2025-06-23 14:39:18 +0200

    Formulaire d'inscription terminé

---
+++ inscription.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+
+<body>
+    <h1>Page d'inscription</h1>
+    <form action="utilisateur.php" method="get">
+        <div>
+            <label for="username">Nom : </label>
+            <input id="username" type="text" placeholder="Votre nom d'utilisateur" />
+        </div>
+        <div>
+            <label for="password">Mot de passe : </label>
+            <input id="password" type="password" placeholder="Votre mot de passe" />
+        </div>
+        <div>
+            <label for="age">Age : </label>
+            <input id="age" type="number" placeholder="Votre âge" min=0 max=150 />
+        </div>
+        <div>
+            <label for="ville">Ville : </label>
+            <select id="ville">
+                <option value="marseille">Marseille</option>
+                <option value="lyon">Lyon</option>
+                <option value="paris">Paris</option>
+            </select>
+        </div>
+        <div>
+            <label for="remarque">Remarque : </label>
+            <textarea id="remarque" cols=50 rows=5></textarea>
+        </div>
+        <div>
+            Genre : 
+            <input type="radio" name="genre" value="homme" /> Homme
+            <input type="radio" name="genre" value="femme" /> femme
+            <input type="radio" name="genre" value="trans homme" /> Trans Homme
+            <input type="radio" name="genre" value="trans femme" /> Trans Femme
+            <input type="radio" name="genre" value="bi-genre" /> Bi genre
+            <input type="radio" name="genre" value="agenre" /> Agenre
+            <input type="radio" name="genre" value="tri-genre" /> Tri-genre
+            <input type="radio" name="genre" value="xenogenre" /> Xenogenre
+            <input type="radio" name="genre" value="Sapiogenre" /> Sapiogenre
+        </div>
+        <div>
+            Sport préféré :
+            <input type="checkbox" name="sport" value="football" /> Football
+            <input type="checkbox" name="sport" value="tennis" /> Tennis
+            <input type="checkbox" name="sport" value="e-sport" /> E-sport
+        </div>
+        <button>
+            S'inscrire
+        </button>
+    </form>
+</body>
+
+</html>
