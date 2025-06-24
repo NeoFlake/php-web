0ee688ec1ad9c3929b183d427bc547d4f998b510
Jonathan <jonathan.schmidt.13@gmail.com>
2025-06-24 11:12:14 +0200

    Vidage de la session

--- test.php
+++ test.php
@@ -1,6 +1,14 @@
 <?php
-    // Démarrer la session
-    session_start();
+// démarrer la session
+session_start();
+$nom_c = "";
+$nom_s = "";
+if (isset($_COOKIE['nom'])) {
+    $nom_c = $_COOKIE['nom'];
+}
+if (isset($_SESSION['nom'])) {
+    $nom_s = $_SESSION['nom'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -15,11 +23,16 @@
 <body>
     <h1>Cookies et sessions</h1>
     <h2>Cookies</h2>
-    <p>Nom : <?php echo $_COOKIE['nom'] ?></p>
-    <p>Nom : <?= $_COOKIE['nom'] ?></p>
+    <p>Nom : <?php echo $nom_c ?></p>
+    <p>Nom : <?= $nom_c ?></p>
     <h2>Sessions</h2>
-    <p>Nom : <?php echo $_SESSION['nom'] ?></p>
-    <p>Nom : <?= $_SESSION['nom'] ?></p>
+    <p>Nom : <?php echo $nom_s ?></p>
+    <p>Nom : <?= $nom_s ?></p>
+    <p>
+        <a href="vider_session.php">
+            Vider la session
+        </a>
+    </p>
 </body>
 
 </html>
